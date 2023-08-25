9215c1f586b37f3f8c7664e205fcac68cdb44a6a
Kabahita Victoria <89969629+KabahitaVictoria@users.noreply.github.com>
2023-08-25 13:18:09 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ function About_Me() {
             <div className="about-me">
                 <h3>About</h3>
                 <p>
-                    I am a student of Computer Science interested mainly in Frontend Website 
-                    Development and Graphic Design. I love designing and creating beautiful 
+                    Student of Computer Science interested mainly in Website 
+                    Development. I love designing and creating beautiful 
                     layouts, as well as interactive web pages. I am always eager to learn more. 
                 </p>
             </div>
@@ -18,7 +18,7 @@ function Interests() {
         <div className="interests">
                 <h3>Interests</h3>
                 <p>
-                    Reading. Food. Living healthy. Computers. A bit of art...And more reading :D
+                    Reading. Learning a new language. Trying my hand at drawing. Music (especially oldies).
                 </p>
         </div>
     )
